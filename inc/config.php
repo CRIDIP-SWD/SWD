f5c7ebb2a09c7d0b9c71c28ad88cb92f6642a069
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-05 22:31:08 +0100

    SWD

--- inc/config.php
+++ inc/config.php
@@ -16,6 +16,7 @@ DEFINE("CONTROL", "control/");
 DEFINE("API", "API/");
 DEFINE("TOKEN", "token/");
 DEFINE("VENDOR", "vendor/");
+DEFINE("SYNCHRONUS", "http://synchronus-media.cridip.com/gestcom/");
 
 //DATE
 $date_jour = date("d-m-Y");
