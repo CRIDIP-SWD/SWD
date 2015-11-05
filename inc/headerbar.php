61ed465411f6d1cd97aec6f56b0a25ec19ea8387
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-05 18:59:14 +0100

    SWD

--- inc/headerbar.php
+++ inc/headerbar.php
@@ -14,29 +14,10 @@
         <ul class="nav navbar-nav nav-main-xs">
             <li><a href="#" class="icon-toolsbar nav-mini"><i class="fa fa-bars"></i></a></li>
         </ul>
-        <ul class="nav navbar-nav nav-top-xs hidden-xs tooltip-area">
-            <li class="h-seperate"></li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"  data-hover="dropdown"><i class="fa fa-th-large"></i></a>
-                <ul class="dropdown-menu arrow animated fadeInDown fast">
-                    <li><a href="#"> Bookmarks</a></li>
-                    <li><a href="#"> Make a Deposit</a></li>
-                    <li class="divider"></li>
-                    <li class="dropdown-submenu"> <a tabindex="-1" href="#">Multi level options &nbsp; <i class="fa fa-angle-right"></i></a>
-                        <ul class="dropdown-menu  animated fadeInRight fast">
-                            <li><a tabindex="-1" href="#">Second level</a></li>
-                            <li><a href="#">Second level</a></li>
-                            <li><a href="#">Second level</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <!-- //dropdown-menu-->
-            </li>
-            <li class="h-seperate"></li>
-            <li><a href="#" data-toggle="tooltip" title="View front end" data-container="body"  data-placement="bottom"><i class="fa fa-laptop"></i></a></li>
+        <!--<ul class="nav navbar-nav nav-top-xs hidden-xs tooltip-area">
             <li class="h-seperate"></li>
             <li><a href="#"> Online Store </a></li>
-        </ul>
+        </ul>-->
         <ul class="nav navbar-nav navbar-right tooltip-area">
             <li><a href="#menu-right" data-toggle="tooltip" title="Right Menu" data-container="body" data-placement="left"><i class="fa fa-align-right"></i></a></li>
             <li class="hidden-xs hidden-sm btn-help"><a href="#" class="h-seperate">Help</a></li>
