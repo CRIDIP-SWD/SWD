2e9403259baec2c6dfe1db98b8a15255e1c07ac4
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-05 22:17:44 +0100

    SWD

--- inc/classe.php
+++ inc/classe.php
@@ -21,6 +21,6 @@ $csk3 = "k1ung5OPmvb26KtO97wy6R85SyL2ZIVU";
 $ovh3 = new Api($apk3,$ask3,$endpoint,$csk3);
 
 
-require_once "../control/user.php";
+require_once "control/user.php";
 $user_cls = new user();
 $user = $user_cls->info_user($login);
