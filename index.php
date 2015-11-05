ddc04ce67594b6d4627633e1030120550275f0ea
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-05 23:37:28 +0100

    SWD

--- index.php
+++ index.php
@@ -9,6 +9,18 @@ if(isset($_GET['view']))
         case "login":
             include "view/login.php";
             break;
+        case "ovh":
+            include "view/ovh.php";
+            break;
+        case "gestion":
+            include "view/gestion.php";
+            break;
+        case "project":
+            include "view/projet.php";
+            break;
+        case "outil":
+            include "view/outil.php";
+            break;
     }
 }else{
     include "view/error404.php";
