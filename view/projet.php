4924fdc0f7007734f3d5db10c6f10f44b3c7876e
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-06 19:50:17 +0100

    SWD

---
+++ view/projet.php
@@ -0,0 +1,45 @@
+<?php
+$nom_sector = "";
+$nom_page = "Bienvenue";
+include "inc/header.php";
+?>
+<body class="leftMenu nav-collapse">
+<div id="wrapper">
+	<?php include "inc/headerbar.php"; ?>
+		<div id="main">
+
+				<ol class="breadcrumb">
+						<li><a href="#"><?= NOM_LOGICIEL; ?></a></li>
+						<?php if(!empty($nom_sector)){echo "<li><a href='#'>".$nom_sector."</a></li>";} ?>
+						<?php if(!empty($nom_page)){echo "<li><a href='#'>".$nom_page."</a></li>";} ?>
+				</ol>
+				<!-- //breadcrumb-->
+
+				<div id="content">
+						
+				</div>
+				<!-- //content-->
+								
+		</div>
+		<!-- //main-->
+		
+		<?php include "inc/sidebar.php"; ?>
+		
+		
+		<?php include "inc/rightbar.php"; ?>
+		
+		
+</div>
+<!-- //wrapper-->
+
+
+<!--
+////////////////////////////////////////////////////////////////////////
+//////////     JAVASCRIPT  LIBRARY     //////////
+/////////////////////////////////////////////////////////////////////
+-->
+		
+	<?php include "inc/script.php"; ?>
+
+</body>
+</html>
