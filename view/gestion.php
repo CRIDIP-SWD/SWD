d6f2ce913b2708d1bb36dc5aecfde1f2bf5ff6f3
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-06 22:03:55 +0100

    SWD

--- view/gestion.php
+++ view/gestion.php
@@ -26,7 +26,7 @@ include "inc/header.php";
 								<div class="widget-tile">
 									<section>
 										<h5><strong>Nombre</strong> de Client </h5>
-										<h2>3</h2>
+										<h2><?= $gen_cls->data_count_client(); ?></h2>
 									</section>
 									<div class="hold-icon"><i class="fa fa-users"></i></div>
 								</div>
