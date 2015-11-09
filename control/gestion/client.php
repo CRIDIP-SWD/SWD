ce9d5cb69e215f7bbc08611e014edcbdb3449bd1
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-09 15:26:34 +0100

    SWD

--- control/gestion/client.php
+++ control/gestion/client.php
@@ -14,7 +14,6 @@ class client
 if(isset($_GET['action']) && $_GET['action'] == 'calling')
 {
     namespace Ovh;
-    use Ovh\Api;
     $apk1 = "vXjHPaL84Jct1zaB";
     $endpoint = "ovh-eu";
     $ask1 = "WwSivyMF8kcmKMlsjd6SRCQsmox8XKnO";
