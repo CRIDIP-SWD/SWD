70d5c4d1dd345420505cb760e803933677cdb961
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-09 14:54:33 +0100

    SWD

---
+++ control/gestion/client.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CRIDIP-SWD
+ * Date: 09/11/2015
+ * Time: 14:31
+ */
+
+
+class client
+{
+
+}
+if(isset($_GET['action']) && $_GET['action'] == 'calling')
+{
+    include ('')
+}
--- view/gestion.php
+++ view/gestion.php
@@ -225,7 +225,7 @@ include "inc/header.php";
 											</div>
 											<button type="button" class="btn btn-info" data-toggle="modal" data-target="#edit-client-modal"><i class="fa fa-edit"></i> Editer le client</button>
 											<button type="button" class="btn btn-danger"><i class="fa fa-remove"></i> Supprimer le client</button>
-											<button type="button" class="btn btn-success"><i class="fa fa-phone"></i></button>
+											<button type="button" class="btn btn-success" onclick="window.location.href='<?= ROOT,CONTROL; ?>'"><i class="fa fa-phone"></i></button>
 										</div>
 										<div class="profile-status">
 											<a class="btn"> 5 <small>Devis</small></a>
