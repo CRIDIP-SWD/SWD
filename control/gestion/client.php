f83a1fd109faf4c8b1a30c452d216ce073c8a43b
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-09 15:13:21 +0100

    SWD

--- control/gestion/client.php
+++ control/gestion/client.php
@@ -14,7 +14,6 @@ class client
 if(isset($_GET['action']) && $_GET['action'] == 'calling')
 {
     require_once('../../vendor/autoload.php');
-    use Ovh\Api;
     $apk1 = "vXjHPaL84Jct1zaB";
     $endpoint = "ovh-eu";
     $ask1 = "WwSivyMF8kcmKMlsjd6SRCQsmox8XKnO";
