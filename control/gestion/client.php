86c73a45dad57deb3ce76b0813cb7bc90ac2dd8a
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-10 16:34:20 +0100

    SWD

--- control/gestion/client.php
+++ control/gestion/client.php
@@ -98,11 +98,11 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-client')
         </div>
         <div class="corps">
             <p>Bonjour,</p>
-            <p>Vous avez fait appel à nos services, et nous vous remercions.</p>
-            <p>Voici un récapitulatif de l'inscription à votre Espace Client CRIDIP, ou vous pourrez:</p>
+            <p>Vous avez fait appel à nos services et nous vous remercions.</p>
+            <p>Voici un récapitulatif de l'inscription à votre Espace Client CRIDIP, où vous pourrez:</p>
             <ul>
                 <li>Avoir accès à vos coordonnées et les modifiés</li>
-                <li>Avoir accès à toutes vos documents contractuel, factures, commandes et devis</li>
+                <li>Avoir accès à tous vos documents contractuel, factures, commandes et devis</li>
             </ul>
             <p>Voici vos identifiants:</p>
             <table class="id">
@@ -136,4 +136,101 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-client')
         header("Location: ../../index.php?view=gestion&sub=client&error=add-client");
     }
 
+}
+if(isset($_POST['action']) && $_POST['action'] == 'edit_client')
+{
+    include "../../inc/config.php";
+    include "../../inc/classe.php";
+
+    $idclient   = $_POST['idclient'];
+    $nom_client     = $_POST['nom_client'];
+    $nom_societe    = $_POST['nom_societe'];
+    $email          = $_POST['email'];
+    $telephone      = $_POST['telephone'];
+    $adresse        = htmlentities(addslashes($_POST['adresse']));
+    $code_postal    = $_POST['code_postal'];
+    $ville          = htmlentities(addslashes($_POST['ville']));
+    $region         = htmlentities(addslashes($_POST['region']));
+
+    $sql_edit_client = mysql_query("UPDATE client SET nom_client = '$nom_client',nom_societe = '$nom_societe',email = '$email',telephone = '$telephone',adresse = '$adresse',code_postal = '$code_postal',ville = '$code_postal',region = '$region'WHERE idclient = '$idclient'")or die(mysql_error());
+
+
+//Email
+    $to = $email;
+    $sujet = "Modification de vos informations - CRIDIP";
+    $headers = 'Mime-Version: 1.0'."\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
+    $headers .= "\r\n";
+
+    ob_start();
+    ?>
+    <html>
+    <head>
+        <link rel="stylesheet" href="http://gestcom.cridip.com/assets/css/modif.css">
+    </head>
+    <body>
+    <div id="email">
+        <div class="header">
+            <div class="logo"><img src="<?= ROOT,ASSETS,IMG; ?>logo_white_icon.png" /></div>
+            <div class="dot-bar"><img src="<?= ROOT,ASSETS,IMG; ?>dot-bar.png" /></div>
+            <div class="adresse">
+                <strong>CRIDIP SWD</strong><br>
+                8 Rue Octave Voyer<br>
+                85100 Les Sables d'Olonne
+            </div>
+            <div class="sujet"><?= $sujet; ?></div>
+        </div>
+        <div class="corps">
+            <p>Bonjour,</p>
+            <p>Vos Informations ont été modifiées.</p>
+            <p>Voici un récapitulatif de la modification apporter:</p>
+            <table style="width: 50%; border: solid 2px #C1C1C1; border-radius: 5px;">
+                <tr>
+                    <td style="width: 50%; font-weight: 700; padding: 5px;">Nom de la société</td>
+                    <td style="width: 50%; padding: 5px;"><?= $nom_societe; ?></td>
+                </tr>
+                <tr>
+                    <td style="width: 50%; font-weight: 700; padding: 5px;">Nom du client</td>
+                    <td style="width: 50%; padding: 5px;"><?= $nom_client; ?></td>
+                </tr>
+                <tr>
+                    <td style="width: 50%; font-weight: 700; padding: 5px;">Adresse Mail</td>
+                    <td style="width: 50%; padding: 5px;"><?= $email; ?></td>
+                </tr>
+                <tr>
+                    <td style="width: 50%; font-weight: 700; padding: 5px;">Numéro de Téléphone</td>
+                    <td style="width: 50%; padding: 5px;">0<?= substr($telephone, 4, 12); ?></td>
+                </tr>
+                <tr>
+                    <td style="width: 50%; font-weight: 700; padding: 5px;">Adresse Postal</td>
+                    <td style="width: 50%; padding: 5px;">
+                        <?= html_entity_decode($adresse); ?><br>
+                        <?= $code_postal; ?> <?= html_entity_decode($ville); ?><br>
+                        <?= html_entity_decode($region); ?>
+                    </td>
+                </tr>
+            </table>
+            <p>Votre identifiant et votre mot de passe reste inchangé.</p>
+            <p>Vous pouvez changer ces informations en allant sur <i>Espace client > Vos Informations</i></p>
+            <p>Cordialement,</p>
+            <p>Le service commercial</p>
+        </div>
+        <div class="footer">
+            <hr />
+            SAS au capital de 100€ - RCS La Roche sur Yon 811 772 235 - Siège social: 8 rue Octave Voyer, 85100 Les Sables d'Olonne - FRANCE
+        </div>
+
+    </div>
+    </body>
+    </html>
+    <?php
+    $msg = ob_get_contents();
+    $mail_envoie = mail($to, $sujet, $msg, $headers);
+
+    if($sql_add_client === TRUE AND $mail_envoie === TRUE)
+    {
+        header("Location: ../../index.php?view=gestion&sub=client&data=view_client&num_client=$num_client&success=add-client");
+    }else{
+        header("Location: ../../index.php?view=gestion&sub=client&error=add-client");
+    }
 }
