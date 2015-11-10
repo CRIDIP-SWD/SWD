fbaea4b1151c493cf719bb9cb189fdcdbbd98cab
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-10 14:37:46 +0100

    SWD

--- control/gestion/client.php
+++ control/gestion/client.php
@@ -87,22 +87,22 @@ if(isset($_GET['action']) && $_GET['action'] == 'calling')
         $nom_client = $client['nom_client'];
     }
 
-    //$soap = new SoapClient("https://www.ovh.com/soapi/soapi-re-1.63.wsdl");
-    //$responder = $soap->telephonyClick2CallDo("mmockelyn", "1992maxime", $num_appelant, $num_appeler, $num_appelant);
+    $soap = new SoapClient("https://www.ovh.com/soapi/soapi-re-1.63.wsdl");
+    $responder = $soap->telephonyClick2CallDo("mmockelyn", "1992maxime", $num_appelant, $num_appeler, $num_appelant);
 
-    $apk1 = "vXjHPaL84Jct1zaB";
+    /*$apk1 = "vXjHPaL84Jct1zaB";
     $endpoint = "ovh-eu";
     $ask1 = "WwSivyMF8kcmKMlsjd6SRCQsmox8XKnO";
     $csk1 = "k1ung5OPmvb26KtO97wy6R85SyL2ZIVU";
-    $ovh1 = new \Ovh\Api($apk1,$ask1,$endpoint,$csk1);
+    $ovh1 = new \Ovh\Api($apk1,$ask1,$endpoint,$csk1);*/
 
-    $content_call = (object) array(
+    /*$content_call = (object) array(
       "calledNumber" => $num_appeler
-    );
+    );*/
 
-    $step = $ovh1->post("/telephony/ovhtel-32816764-1/line/0033972527971/click2Call", $content_call);
+    //$step = $ovh1->post("/telephony/ovhtel-32816764-1/line/0033972527971/click2Call", $content_call);
 
-    if($step === NULL)
+    if($responder === NULL)
     {
         $sql_called = mysql_query("INSERT INTO `client_called`(`idclientcalled`, `num_user`, `num_client`, `nom_client`, `date_appel`, `status`)
                                 VALUES (NULL,'$num_appelant','$num_appeler','$nom_client','$date_jour_heure_strt','1')")or die("ERROR SQL: ".mysql_error());
