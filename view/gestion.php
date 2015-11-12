d20b78547f82fc5d97af148a54487efdd8b288c9
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-12 11:41:42 +0100

    SWD

--- view/gestion.php
+++ view/gestion.php
@@ -1010,7 +1010,7 @@ include "inc/header.php";
 							<div class="col-md-6">
 								<section class="panel">
 									<header class="panel-heading">
-										<h3>CCARACTERISTIQUE </h3>
+										<h3>CARACTERISTIQUE </h3>
 									</header>
 									<div class="panel-body">
 										<div class="pull-right">
@@ -1044,6 +1044,129 @@ include "inc/header.php";
 								</section>
 							</div>
 						</div>
+					</div>
+					<div id="add-caracteristique" data-width="700" class="modal fade">
+						<div class="modal-header bg-success-gradient">
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
+							<h4 class="modal-title"><i class="fa fa-plus"></i> Ajouter une caractéristique</h4>
+						</div>
+						<!-- //modal-header-->
+						<form class="form-horizontal" action="<?= ROOT,CONTROL; ?>gestion/article.php" method="post">
+							<input type="hidden" name="idarticle" value="<?= $idarticle; ?>" />
+							<div class="modal-body">
+
+								<div class="form-group">
+									<label class="control-label col-md-3" style="text-align: left;">Caractéristique</label>
+									<div class="col-md-9">
+										<input type="text" class="form-control rounded" name="caracteristique">
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="control-label col-md-3" style="text-align: left;">Valeur</label>
+									<div class="col-md-9">
+										<input type="text" class="form-control rounded" name="value">
+									</div>
+								</div>
+
+
+							</div>
+							<!-- //modal-body-->
+							<div class="modal-footer bg-success-gradient">
+								<button type="submit" class="btn btn-default pull-right" name="action" value="add-caracteristique"><i class="fa fa-check"></i> Valider</button>
+							</div>
+						</form>
+					</div>
+					<div id="edit-article" data-width="1400" class="modal fade">
+						<div class="modal-header bg-success-gradient">
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
+							<h4 class="modal-title"><i class="fa fa-plus"></i> Ajouter un article</h4>
+						</div>
+						<!-- //modal-header-->
+						<form class="form-horizontal" action="<?= ROOT,CONTROL; ?>gestion/article.php" method="post">
+							<div class="modal-body">
+
+								<div class="form-group">
+									<label class="control-label"> Type de Produit</label>
+									<div>
+										<div class="row">
+											<div class="col-sm-12">
+												<ul class="iCheck" data-color="red">
+													<li>
+														<input type="radio" name="type_article" value="1" <?php if($article['type_article'] == 1){echo "checked='checked'";} ?>>
+														<label><i class="fa fa-globe"></i> Web</label>
+													</li>
+													<li>
+														<input  type="radio" name="type_article" value="2" <?php if($article['type_article'] == 1){echo "checked='checked'";} ?>>
+														<label ><i class="fa fa-server"></i> Serveur</label>
+													</li>
+													<li>
+														<input  type="radio" name="type_article" value="3" <?php if($article['type_article'] == 1){echo "checked='checked'";} ?>>
+														<label ><i class="fa fa-phone-square"></i> Télécom</label>
+													</li>
+												</ul>
+											</div><!-- //col-sm-6 -->
+										</div><!-- //row-->
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="control-label col-md-3">Famille</label>
+									<div class="col-md-9">
+										<select  class="selectpicker form-control rounded" name="famille" data-size="10" data-live-search="true">
+											<?php
+											$sql_default = mysql_query("SELECT * FROM swd_famille_article WHERE idfamillearticle = ".$article['famille'])or die(mysql_error());
+											$default = mysql_fetch_array($sql_default);
+											?>
+											<option value="<?= $default['idfamillearticle']; ?>"><?= $default['designation_famille']; ?></option>
+											<?php
+											$sql_famille = mysql_query("SELECT * FROM swd_famille_article")or die(mysql_error());
+											while($famille = mysql_fetch_array($sql_famille))
+											{
+												?>
+												<option value="<?= $famille['idfamillearticle']; ?>"><?= $famille['designation_famille']; ?></option>
+											<?php } ?>
+										</select>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="control-label col-md-3">Nom de l'article</label>
+									<div class="col-md-9">
+										<input type="text" class="form-control rounded" name="nom_article" value="<?= html_entity_decode($article['nom_article']); ?>">
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="control-label col-md-3">Courte description</label>
+									<div class="col-md-9">
+										<textarea class="form-control" data-provide="markdown" rows="3" maxlength="255"  data-always-show="true" placeholder="Tapez un courte description de l'article" data-pre-text='Il vous reste ' data-post-text=' caractère' name="short_description"><?= html_entity_decode($article['short_description']); ?></textarea>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="control-label col-md-3">Description</label>
+									<div class="col-md-9">
+										<textarea class="form-control" data-provide="markdown" rows="3" name="long_description"><?= html_entity_decode($article['long_description']); ?></textarea>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="control-label col-md-3">Prix</label>
+									<div class="col-md-9">
+										<div class="input-group rounded"> <span class="input-group-addon">€</span>
+											<input type="text" name="prix_vente_ht" class="form-control rounded" value="<?= $article['prix_vente_ht']; ?>">
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+							<!-- //modal-body-->
+							<div class="modal-footer bg-success-gradient">
+								<button type="submit" class="btn btn-default pull-right" name="action" value="add-article"><i class="fa fa-check"></i> Valider</button>
+							</div>
+						</form>
 					</div>
 				<?php } ?>
 			<?php } ?>
