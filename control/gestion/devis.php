3b158d7631633ea26891fb1d6a3ac54110466897
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-12 15:10:10 +0100

    SWD

--- control/gestion/devis.php
+++ control/gestion/devis.php
@@ -6,7 +6,7 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-devis')
     
     $idclient = $_POST['idclient'];
     $reference = "DEVSWD2015".date('n').rand(1,9999);
-    $date_devis = $_POST['date_devis'];
+    $date_devis = strtotime($_POST['date_devis']);
     $date_echeance = $date_devis + $_POST['echeance'];
     
     $sql_add_devis = mysql_query("INSERT INTO swd_devis(`iddevis`, `reference`, `etat_devis`, `date_devis`, `date_echeance`, `idclient`, `total_ht`) VALUES (NULL, '$reference', '1', '$date_devis', '$date_echeance', '$idclient', '0')")or die(mysql_error());
