d813ea91b87210b946834a7726654827d43362a1
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-14 00:42:34 +0100

    SWD

--- control/gestion/facture.php
+++ control/gestion/facture.php
@@ -325,6 +325,8 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-reglement')
             'HDRIMG'                            => ROOT.ASSETS.IMG."logo_2x.png",
             'EMAIL'                             => $email,
             'BRANDNAME'                         => "CRIDIP SWD X1.00",
+            'L_PAYMENTREQUEST_0_NAME0'          => $reference,
+            'L_PAYMENTREQUEST_0_AMT0'           => $montant_reglement,
         );
         $response = $paypal_cls->request('SetExpressCheckout', $params);
         if($response){
