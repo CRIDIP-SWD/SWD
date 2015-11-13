b400531c3805ea6ab2a89b7648d5123612f42458
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-13 23:57:22 +0100

    SWD

--- control/gestion/facture.php
+++ control/gestion/facture.php
@@ -322,7 +322,7 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-reglement')
             'PAYMENTREQUEST_0_CURRENCYCODE' => 'EUR',
         );
         $sql_article = mysql_query("SELECT * FROM swd_facture_ligne, swd_article WHERE swd_facture_ligne.idarticle = swd_article.idarticle AND idfacture = '$idfacture'")or die(mysql_error());
-        $products = mysql_fetch_array($sql_article, MYSQL_ASSOC);
+        $products = mysql_fetch_assoc($sql_article);
         foreach($products as $k => $product){
             $params["L_PAYMENTREQUEST_0_NAME$k"] = $product['nom_article'];
             $params["L_PAYMENTREQUEST_0_DESC$k"] = '';
