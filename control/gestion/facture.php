c94c7bba95f7b8ea45bac15a9d6e6baea9535a52
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-14 00:17:15 +0100

    SWD

--- control/gestion/facture.php
+++ control/gestion/facture.php
@@ -324,10 +324,10 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-reglement')
         $sql_article = mysql_query("SELECT * FROM swd_facture_ligne, swd_article WHERE swd_facture_ligne.idarticle = swd_article.idarticle AND idfacture = '$idfacture'")or die(mysql_error());
         while($products = mysql_fetch_assoc($sql_article)) {
             foreach($products as $k => $product){
-                $params["L_PAYMENTREQUEST_0_NAME$k"] = $product[0]["nom_article"];
+                $params["L_PAYMENTREQUEST_0_NAME$k"] = $product[$k]["nom_article"];
                 $params["L_PAYMENTREQUEST_0_DESC$k"] = '';
-                $params["L_PAYMENTREQUEST_0_AMT$k"] = $product[0]["total_ligne"];
-                $params["L_PAYMENTREQUEST_0_QTY$k"] = $product[0]["qte"];
+                $params["L_PAYMENTREQUEST_0_AMT$k"] = $product[$k]["total_ligne"];
+                $params["L_PAYMENTREQUEST_0_QTY$k"] = $product[$k]["qte"];
             }
             print_r($products);
         }
