76d4d09ad40f1f78e0dd4bd4fc203f71b99c07f6
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-15 18:36:15 +0100

    SWD

--- control/gestion/facture.php
+++ control/gestion/facture.php
@@ -359,7 +359,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'active-paypal')
             'PAYERID' => $_GET['PayerID'],
             'PAYMENTACTION' => 'Sale',
 
-            'PAYMENTREQUEST_0_AMT'              => $_GET['AMT'],
+            'PAYMENTREQUEST_0_AMT'              => $_GET['PAYMENTREQUEST_0_AMT'],
             'PAYMENTREQUEST_0_CURRENCYCODE'     => 'EUR',
         );
         $response = $paypal->request('DoExpressCheckoutPayment', $params);
