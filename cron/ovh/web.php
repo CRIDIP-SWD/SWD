7c1f1a0142183815d3c5890af3942b87b0538518
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-16 23:58:34 +0100

    SWD

--- cron/ovh/web.php
+++ cron/ovh/web.php
@@ -7,6 +7,6 @@ $domaine1 = $ovh1->get("/domain");
 $domaine2 = $ovh2->get("/domain");
 $domaine3 = $ovh3->get("/domain");
 
-var_dump($domaine1);
-var_dump($domaine2);
-var_dump($domaine3);
+foreach ($domaine1 as $k => $domaine) {
+    echo $domaine[$k];
+}
