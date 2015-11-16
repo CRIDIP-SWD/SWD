bc1fb553f0e0d6a539fe31e9734945adbb8a0971
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-16 23:53:06 +0100

    SWD

--- cron/ovh/web.php
+++ cron/ovh/web.php
@@ -3,3 +3,8 @@ include "../../inc/config.php";
 include "../../inc/classe.php";
 
 // RECUPERATION DOMAINE
+$domaine = $ovh1->get("/domain");
+$domaine .= $ovh2->get("/domain");
+$domaine .= $ovh3->get("/domaine");
+
+var_dump($domaine);
