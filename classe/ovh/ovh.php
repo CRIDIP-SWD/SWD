2c70f9411381e88a1a1684e18679c1b90248a05e
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-16 22:42:18 +0100

    SWD

---
+++ classe/ovh/ovh.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: MAX
+ * Date: 16/11/2015
+ * Time: 22:12
+ */
+class ovh
+{
+
+}
--- view/ovh.php
+++ view/ovh.php
@@ -16,7 +16,25 @@ include "inc/header.php";
 				<!-- //breadcrumb-->
 
 				<div id="content">
-						
+					<?php if(!isset($_GET['sub'])){ ?>
+						<div class="row">
+							<div class="col-md-2">
+								<div class="well bg-info">
+									<div class="widget-tile">
+										<section>
+											<h5><strong>REGISTERED</strong> USERS </h5>
+											<h2>8,590</h2>
+											<div class="progress progress-xs progress-white progress-over-tile">
+												<div class="progress-bar  progress-bar-white" aria-valuetransitiongoal="8590" aria-valuemax="10000"></div>
+											</div>
+											<label class="progress-label label-white"> Just 1410 member to limit , <a href="#">Upgrade Now</a> </label>
+										</section>
+										<div class="hold-icon"><i class="fa fa-bar-chart-o"></i></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					<?php } ?>
 				</div>
 				<!-- //content-->
 								
