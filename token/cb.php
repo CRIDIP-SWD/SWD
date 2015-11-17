40863e838c9098cd8e969d21e5bdb7f879d1d76c
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-17 11:35:07 +0100

    SWD

--- token/cb.php
+++ token/cb.php
@@ -1,9 +1,36 @@
 <?php
 $nom_sector = "";
 $nom_page = "Bienvenue";
-include "../inc/header.php";
 
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+	<!-- Meta information -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+	<!-- Title-->
+	<title><?= NOM_LOGICIEL; ?> - <?= $nom_page; ?></title>
+
+	<!-- Favicons -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= ROOTS,ASSETS,ICO; ?>apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= ROOTS,ASSETS,ICO; ?>apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= ROOTS,ASSETS,ICO; ?>apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="<?= ROOTS,ASSETS,ICO; ?>apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="<?= ROOTS,ASSETS,ICO; ?>favicon.ico">
+
+	<!-- CSS Stylesheet-->
+	<link type="text/css" rel="stylesheet" href="<?= ROOTS,ASSETS,CSS; ?>bootstrap/bootstrap.min.css" />
+	<link type="text/css" rel="stylesheet" href="<?= ROOTS,ASSETS,CSS; ?>bootstrap/bootstrap-themes.css" />
+	<link type="text/css" rel="stylesheet" href="<?= ROOTS,ASSETS,CSS; ?>style.css" />
+
+	<!-- Styleswitch if  you don't chang theme , you can delete -->
+	<link type="text/css" rel="alternate stylesheet" media="screen" title="style4" href="<?= ROOTS,ASSETS,CSS; ?>styleTheme4.css" />
+	<link rel="stylesheet" href="<?= ROOTS,ASSETS,PLUGINS; ?>toastr/toastr.css">
+
+</head>
 <body class="leftMenu nav-collapse">
 <div id="wrapper">
 		<div id="main">
--- token/facture.php
+++ token/facture.php
@@ -14,20 +14,20 @@ $nom_page = "Bienvenue";
 	<title><?= NOM_LOGICIEL; ?> - <?= $nom_page; ?></title>
 
 	<!-- Favicons -->
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= ROOT,ASSETS,ICO; ?>apple-touch-icon-144-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= ROOT,ASSETS,ICO; ?>apple-touch-icon-114-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= ROOT,ASSETS,ICO; ?>apple-touch-icon-72-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" href="<?= ROOT,ASSETS,ICO; ?>apple-touch-icon-57-precomposed.png">
-	<link rel="shortcut icon" href="<?= ROOT,ASSETS,ICO; ?>favicon.ico">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= ROOTS,ASSETS,ICO; ?>apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= ROOTS,ASSETS,ICO; ?>apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= ROOTS,ASSETS,ICO; ?>apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="<?= ROOTS,ASSETS,ICO; ?>apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="<?= ROOTS,ASSETS,ICO; ?>favicon.ico">
 
 	<!-- CSS Stylesheet-->
-	<link type="text/css" rel="stylesheet" href="<?= ROOT,ASSETS,CSS; ?>bootstrap/bootstrap.min.css" />
-	<link type="text/css" rel="stylesheet" href="<?= ROOT,ASSETS,CSS; ?>bootstrap/bootstrap-themes.css" />
-	<link type="text/css" rel="stylesheet" href="<?= ROOT,ASSETS,CSS; ?>style.css" />
+	<link type="text/css" rel="stylesheet" href="<?= ROOTS,ASSETS,CSS; ?>bootstrap/bootstrap.min.css" />
+	<link type="text/css" rel="stylesheet" href="<?= ROOTS,ASSETS,CSS; ?>bootstrap/bootstrap-themes.css" />
+	<link type="text/css" rel="stylesheet" href="<?= ROOTS,ASSETS,CSS; ?>style.css" />
 
 	<!-- Styleswitch if  you don't chang theme , you can delete -->
-	<link type="text/css" rel="alternate stylesheet" media="screen" title="style4" href="<?= ROOT,ASSETS,CSS; ?>styleTheme4.css" />
-	<link rel="stylesheet" href="<?= ROOT,ASSETS,PLUGINS; ?>toastr/toastr.css">
+	<link type="text/css" rel="alternate stylesheet" media="screen" title="style4" href="<?= ROOTS,ASSETS,CSS; ?>styleTheme4.css" />
+	<link rel="stylesheet" href="<?= ROOTS,ASSETS,PLUGINS; ?>toastr/toastr.css">
 
 </head>
 <?php
